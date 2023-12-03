153dc3a9dbdd88710a874bbf878d1434afc85f9f
ismet efe balik <ibalik@k1m05s03.42istanbul.com.tr>
2023-12-03 07:58:39 +0300

    first

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointValue(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+    std::cout << "Assignation operator called" << std::endl;
+    this->fixedPointValue = fixed.getRawBits();
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    this->fixedPointValue = raw;
+}
